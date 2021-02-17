a456bf1b4dd9ead06054fcb943ec8ba3f1a1f18d
Nick Kessey <nkessey@umich.edu>
2021-02-16 22:57:25 -0500

    Project Initialization

---
+++ CIS476_P1/CIS476_P1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CIS476_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
